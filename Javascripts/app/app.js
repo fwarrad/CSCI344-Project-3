5f0c8c11b934a3bf7af9fd53c2acf006b25c5ab3
Fareed Warrad <fwarrad@unca.edu>
2013-03-22 00:10:15 -0400

    final commit

--- Javascripts/app/app.js
+++ Javascripts/app/app.js
@@ -2,27 +2,62 @@ var $ = window.$;
 var ctwitter = window.ctwitter; 
 var console = window.console; 
 var main = function () {
+    "use strict";
     //variable count
-    var itemNum = 0;
-    var totalTasks = 0;
-    var numTasks = 0;
-    var categTasks = 0;
+    var totalTasks = 0,
+        categTasks = 0;
+
     //Show & Hide
     $(".tabs").hide();
     $(".edit_tab_content").hide();
     $(".resetButton").hide();
     $("#startButton").click(function () {
-      $(".startButton").hide();
-      $(".resetButton").show();
-      $(".tabs").delay(80000000).fadeIn(80000000).show();
-      $(".edit_tab_content").delay(80000000).fadeIn(80000000).show();
+        $(".startButton").hide();
+        $(".resetButton").show();
+        $(".tabs").delay(80000000).fadeIn(80000000).show();
+        $(".edit_tab_content").delay(80000000).fadeIn(80000000).show();
     }); // end of .click function  
+  //Function that generates a unique ID. 
+    var PseudoGuid = new (function() {
+            this.empty = "00000000-0000-0000-0000-000000000000";
+            this.GetNew = function() {
+                var fourChars = function() {
+                        return (((1 + Math.random()) * 0x10000) | 1).toString(16).substring(1).toUpperCase();
+                    }
+                return (fourChars() + fourChars() + "-" + fourChars() + "-" + fourChars() + "-" + fourChars() + "-" + fourChars() + fourChars() + fourChars());
+    };
+  })();
+
+  //Variable calls for the unique ID.
+  itemNum = PseudoGuid.GetNew();
+  numTasks = 0;
+  allTasks = 0; 
+    
+    var addTask = function (desc, categories) {
+        $("#All").append("<div id='" + itemNum + "'' class='item " + allTasks + "'' >"
+            + "<p class='description'>" + desc + "</p>"
+            + "<p class='categories'>" + categories + "</p>"
+            + "<button type='button' class='remove' id='" + itemNum + "'>Remove</button>"
+            + "</div>");
+            
+        $("#" + itemNum).click(function () {
+            console.log("removed task from All Tab");
+            var taskRemoved = $(this).attr("id");
+            $("#" + taskRemoved).remove();
+            totalTasks--;
+        });
+        itemNum+= 1;
+        allTasks+= 1; 
+        totalTasks++;
+        
+    };
     
     var buildCategorized = function () {
         //Remove old content
         $("#Categorized").children().remove();
         //build new content
         var x, y; 
+        var numTasks = 0;
         for (x = 0; x < totalTasks; x++) {
             var description = $(".description:eq(" + x + ")").html();
             var categoriesStr = $(".categories:eq(" + x + ")").html();
@@ -42,17 +77,19 @@ var main = function () {
                             + categories[y]
                             + "</h4><p class='description'>"
                             + description
+                            + "</p><p class='remove'>"
                             + "<button type='button' class='remove' id='" + numTasks + "'>Remove</button>"
                             + "</p></div>");
 
-                            $("#"+ numTasks).click(function () {
-                              console.log("removed task from Category Tab");
-                              var taskRemoved = $(this).attr("id");
-                              $("." + taskRemoved).remove();
-                              categTasks--;  
+                            $("#" + numTasks).click(function () {
+                              console.log("Removed task from Category tab.");
+                              var categTaskRemoved = $(this).attr("id");
+                              $("." + categTaskRemoved).remove(); 
+                              totalTasks--;
+                              totalTasks++; 
                             });
-                           numTasks++;
-                           categTasks++;
+                            numTasks+= 1;
+                           
                     }
                 }
             }
@@ -90,24 +127,7 @@ var main = function () {
         });
         
     };
-    
-    var addTask = function (desc, categories) {
-        $("#All").append("<div class='item " + itemNum + "''>"
-            + "<p class='description'>" + desc + "</p>"
-            + "<p class='categories'>" + categories + "</p>"
-            + "<button type='button' class='remove' id='" + itemNum + "'>Remove</button>"
-            + "</div>");
-        $("#" + itemNum).click(function () {
-            console.log("removed task from All Tab");
-            var taskRemoved = $(this).attr("id");
-            $("." + taskRemoved).fadeOut('slow').remove();
-            totalTasks--;
-        });
-        itemNum++;
-        numTasks++
-        totalTasks++;
-    };
-    
+
     var jsonInitializer = function () {
         $(".welcome").show();
         $.getJSON("javascripts/lib/all.json", function (todos) {
